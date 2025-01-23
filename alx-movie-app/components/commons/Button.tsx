942f28bf651359dd40626239689f6158b91bee15
front-end-geek <lovepreetbrands@gmail.com>
2025-01-23 13:28:24 +0300

    task 2

---
+++ alx-movie-app/components/commons/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button: React.FC = () => {
+  return (
+    <div>
+      <button>
+        click me
+      </button>
+    </div>
+  )
+}
+
+export default  Button
---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieCard: React.FC = () => {
+  return (
+    <div>
+      MovieCatd
+    </div>
+  )
+}
+
+export default MovieCard
